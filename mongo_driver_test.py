b52275667734c847659ac4ac3029ee53716158a1
AbdelrhmanBassiouny <abdelrhman_bassuny@hotmail.com>
2023-02-06 15:17:23 +0100

    use environment variable for authentication

--- mongo_driver_test.py
+++ mongo_driver_test.py
@@ -1,8 +1,11 @@
-import pymongo
+import os
+from pymongo import MongoClient
+
+
 # Replace the uri string with your MongoDB deployment's connection string.
-conn_str = "mongodb://neemReader:qEWRqc9UdN5TD7No7cjymUA8QEweNz@neem-3.informatik.uni-bremen.de:28015/neems"
+MONGODB_URI = os.environ["MONGODB_URI"]
 # set a 5-second connection timeout
-client = pymongo.MongoClient(conn_str, serverSelectionTimeoutMS=5000)
+client = MongoClient(MONGODB_URI, serverSelectionTimeoutMS=5000)
 try:
     print(client.server_info())
 except Exception:
